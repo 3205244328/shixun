a4cc72976ac4eb77e46a43fbf5866fc8c5c60e38
3205244328 <3205244328@qq.com>
2021-01-22 15:21:27 +0800

    aaa

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from "@angular/core";
-
 @Injectable()
 export class AuthService {
-    isLoggedIn = false;
-    login() {
-        this.isLoggedIn = true;
-    }
-    logout() {
-        this.isLoggedIn = false;
-    }
-    loggedIn() {
-        return this.isLoggedIn;
-    }
+isLoggedIn = false;
+login() {
+this.isLoggedIn = true;
+}
+logout() {
+this.isLoggedIn = false;
+}
+loggedIn() {
+return this.isLoggedIn;
+}
 }
